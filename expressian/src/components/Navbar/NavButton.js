f736ef1f6da4ce6da1d0b8135769809d3d92858d
Leah_Grace <leahgdg@hotmail.com>
2022-03-10 21:52:13 -0500

    "Updated Navbar"

--- expressian/src/components/Navbar/NavButton.js
+++ expressian/src/components/Navbar/NavButton.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import {NavLink, Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const NavButton = (props) => {
   // const [display, setDisplay] = useState(false);
   const [hover, setHover] = useState(false);
-
+/*
   const style = {
     background: "transparent",
     // margin: ".2em",
@@ -21,14 +22,38 @@ const NavButton = (props) => {
     opacity: hover ? "60%" : "100%",
     margin: '0px 10px',
   }
-
+*/
   return (
-    <NavLink to={props.to} style={{...style, ...props.style}} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+    <NavLink
+        to={props.to}
+            style={{
+            background: 'transparent',
+            textDecoration: 'none',
+            fontSize: '1.25em',
+            color: hover ? "#f49cbc" : '#6f2960',
+            fontWeight: 600,
+            textShadow: '1px 1px #2fbe9b',
+            textAlign: 'center',
+            whiteSpace: 'nowrap',
+            margin: '0 10px',
+            opacity: hover ? "60%" : "100"
+
+    }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+    >
       {props.label}
     </NavLink>
+
   )
   // <a href="destination">text </a>
   // <Link to="destination">text</Link>
 }
+
+NavButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired
+}
+
 
 export default NavButton;
--- expressian/src/components/Routers/AppRouter.js
+++ expressian/src/components/Routers/AppRouter.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from '../Home/Home';
+import Navbar from "../Navbar/Navbar";
 //import components to show
 
 const AppRouter = () => {
     return (
-        <div style={{width: "100%"}}>
+        <div style={{width: "100%", flexDirection: "column"}}>
             {/* Any components that should always be visible go here*/}
             {/* NavBar*/}
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
