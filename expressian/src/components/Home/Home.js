36bc191d9c561e313af98aad3c9a22dcdf046ebc
Leah_Grace <leahgdg@hotmail.com>
2022-03-10 21:51:39 -0500

    "created Splash component"

--- expressian/src/components/Home/Home.js
+++ expressian/src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Container from '../common/Container';
-import Button from '../common/Button'
-import FaButton from '../faCommon/FaButton'
+import Splash from '../common/Splash';
+import splashImg from '../../assets/Splash.jpg';
+import Button from '../common/Button';
+import FaButton from '../faCommon/FaButton';
 
 const Home = () => {
   return (
-    <Container style={{backgroundColor: "#238C72"}}>
-        <h1>Welcome to Expressian Car Rental!</h1>
-        <h2>All cars have coffee holders</h2>
+    <Container style={{backgroundColor: "#ffffff"}}>
+    <Splash
+        image={splashImg} style={{color: "#c9426d"}}>
+         <h1 style={{textShadow: '1px 1px #6f2960'}}>Welcome to Expressian Car Rental!</h1>
+         <h2 style={{textShadow: '1px 1px #6f2960'}}>All cars have coffee holders</h2>
+        </Splash>
+
     </Container>
   )
 }
