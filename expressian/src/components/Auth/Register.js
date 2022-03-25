0664535557ef998f46820c883efd61d6106161eb
Leah Grace <Leahgdg@hotmail.com>
2022-03-24 23:48:50 -0400

    "Refactored for cleanliness and style"

--- expressian/src/components/Auth/Register.js
+++ expressian/src/components/Auth/Register.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import NewUserForm from './NewUserForm';
 import Container from '../common/Container';
-import LoginForm from './LoginForm';
 import Splash from '../common/Splash';
 import splashImg from '../../assets/SplashParallel.jpg';
 import { apiHostUrl } from '../../config'
