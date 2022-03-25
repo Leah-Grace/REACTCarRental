1e62a2a4c1772ac5db9604e65ef0328d71c86f93
Leah Grace <Leahgdg@hotmail.com>
2022-03-24 23:54:46 -0400

    "login form for login page : newLogin, onChange and onSubmit"

--- expressian/src/components/Auth/Login.js
+++ expressian/src/components/Auth/Login.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 import LoginForm from './LoginForm';
 import Container from '../common/Container';
+import Splash from '../common/Splash';
+import splashImg from '../../assets/SplashParallel.jpg';
+import { apiHostUrl } from '../../config';
+import {AuthContext} from '../Providers/AuthProvider'
+
+//import login gtom Register???
 
 const Login = (props) => {
+
+const [newLogin, setNewLogin] = useState({
+    email: '',
+    password: ''
+});
+//Local storage can be used in useState
+
+const [auth, setAuth] = useContext(AuthContext);
+const navigate = useNavigate();
+
+
+const updateForm = (field, value) => {
+    setNewLogin({
+        ...newLogin,
+        [field]: value
+    })
+    }
+
+const onSubmit = (e) => {
+    alert("Submit clicked");
+    const data = newLogin;
+    login(data);
+}
+
+const login = async (data) => {
+    try {
+        const res = await axios.post(`${apiHostUrl}/api/auth/signin`, data);
+        console.log(res.data);
+        setAuth({
+            token: res.data.token,
+            profile: {},
+            roles: res.data.roles
+        })
+    navigate("/");
+    } catch (err) {
+        console.log(err.response ? err.response.data : err.message)
+    }
+}
+
     return (
-        <Container>
+       <Container>
+       <Splash image={splashImg} style={{
+            height: '30vh',
+            color: '#F1F1F1'
+            }}>
         <h1>Login</h1>
-        <LoginForm />
+        </Splash>
+        <LoginForm
+            newLogin={newLogin}
+            onChange={updateForm}
+            onSubmit={onSubmit}
+            />
         </Container>
+
     )
 }
 
--- expressian/src/components/Auth/LoginForm.js
+++ expressian/src/components/Auth/LoginForm.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import Container from '../common/Container';
+import Form from '../common/Form';
+import InlineInputContainer from '../common/InlineInputContainer';
+import Input from '../common/Input';
+import Button from '../common/Button';
+
 
 const LoginForm = (props) => {
+    const {newLogin} = props;
+
+    const handleChange = (e) => {
+        props.onChange(e.target.id, e.target.value);
+    }
+
+
     return (
-        <h1>LoginForm</h1>
+    <Container>
+        <Form onSubmit={props.onSubmit} style={{marginTop: '1em'}} >
+            <InlineInputContainer>
+                <Input
+                    name="username"
+                    id="username"
+                    value={newLogin.username}
+                    placeholder={"Email"}
+                    onChange={handleChange}
+                    type="email"
+                    required
+                />
+            </ InlineInputContainer>
+            <InlineInputContainer>
+                <Input
+                    name="password"
+                    id="password"
+                    value={newLogin.password}
+                    placeholder="Password"
+                    onChange={handleChange}
+                    type="password"
+                    required
+                />
+            </ InlineInputContainer>
+            <Button>Submit</Button>
+        </ Form>
+        </ Container>
     )
 }
 
