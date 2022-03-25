fd153f042452f6d0e7caa36cdf740ae186c7f9b2
Leah Grace <Leahgdg@hotmail.com>
2022-03-24 23:52:36 -0400

    "Included auth.token"

--- expressian/src/components/Home/Home.js
+++ expressian/src/components/Home/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { AuthContext } from '../Providers/AuthProvider';
 import Container from '../common/Container';
 import Splash from '../common/Splash';
 import splashImg from '../../assets/Splash.jpg';
 import Button from '../common/Button';
 import FaButton from '../faCommon/FaButton';
 
+
 const Home = () => {
+    const [auth] = useContext(AuthContext)
   return (
     <Container style={{backgroundColor: "#ffffff"}}>
     <Splash
@@ -15,8 +18,8 @@ const Home = () => {
             }}>
          <h1 style={{textShadow: '1px 1px #6f2960'}}>Welcome to Expressian Car Rental!</h1>
          <h2 style={{textShadow: '1px 1px #6f2960'}}>All cars have coffee holders</h2>
+            <h2>{auth.token}</h2>
         </Splash>
-
     </Container>
   )
 }
